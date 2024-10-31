4973917e0ef7c8fa2f667bcd1ea323b3ebc1691d
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-10-31 17:55:48 +0300

    routing

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loginpage = () => {
+  return (
+    <div>login page</div>
+  )
+}
+
+export default Loginpage
---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata  } from "next"
+type Props = {
+  params :{
+    productId: string
+  }
+}
+export const generateMetadata = ({params}: Props ): Metadata => {
+  return {
+    title: `Product ${params.productId}`
+}
+}
+export default function ProductId ( {params } : Props) {
+  return (
+    <h1>Details about product {params.productId}</h1>
+  )
+}
