8bad433b585e7f2ae1417b55da6725e8328e3b06
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-10-31 23:14:00 +0300

    active

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,25 +1,32 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavLinks = [
-  {name: "Register", href: "/signup"},
-  {name: "Login", href: "/login"},
-  {name: "Forgot Password", href: "/forgot-password"},
-]
-export default function AuthLayout ({
+  { name: "Register", href: "/signup" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return(
+  const pathname = usePathname();
+
+  return (
     <>
-    {NavLinks.map((link)=>(
-      <Link href={link.href} key={link.name}>
-        {link.name}
-      </Link>
-    ))}
-     <body>
-          {children}
-    </body>
+      {NavLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
+        return (
+          <Link href={link.href} key={link.name} className={isActive ? "text-blue-800" : "text-black"}>
+            {link.name}
+          </Link>
+        );
+      })}
+      <div>{children}</div>
     </>
-  )
+  );
 }
